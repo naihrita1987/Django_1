20401648bbdf58e1d08965e60d2875a1a97c579d
Naihrita Munshi <81513753+naihrita1987@users.noreply.github.com>
2021-05-24 17:05:52 +0200

    first commit

--- My_Django/Project_2/AppTwo/views.py
+++ My_Django/Project_2/AppTwo/views.py
@@ -6,4 +6,5 @@ def index(request):
     #return HttpResponse("<em>My Second App</em>")
     my_dict = {"insert_me": "This is my new views.."}
     return render(request,"help.html",context=my_dict)
+    #I have made some changes 
 
