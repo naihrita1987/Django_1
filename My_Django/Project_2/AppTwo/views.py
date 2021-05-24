fcdcc3b782cd3695054d1b67f571c8e0465cf8b7
Naihrita Munshi <81513753+naihrita1987@users.noreply.github.com>
2021-05-24 16:24:14 +0200

    First commit

---
+++ My_Django/Project_2/AppTwo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    #return HttpResponse("<em>My Second App</em>")
+    my_dict = {"insert_me": "This is my new views.."}
+    return render(request,"help.html",context=my_dict)
+
